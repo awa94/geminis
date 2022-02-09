be9c9de292df9b8f543003a24709e7f1688ddc94
awa94 <iris.ledesma.27@gmail.com>
2022-02-09 23:53:35 +0100

    add header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import { AppBar, Toolbar } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>It is a basic list!</Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
